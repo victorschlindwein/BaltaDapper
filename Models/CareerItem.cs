4f186dc804ffdd09a8cc7010c66f66c6e0ea8b58
dizzysch <victorwilbert@gmail.com>
2023-10-19 20:45:15 -0300

    repo finalizado

---
+++ Models/CareerItem.cs
@@ -0,0 +1,9 @@
+namespace BaltaDataAccess.Models
+{
+    public class CareerItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Course Course { get; set; }
+    }
+}
